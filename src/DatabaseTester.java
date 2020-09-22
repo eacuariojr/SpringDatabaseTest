49e85171f1945d5996435d36cc951b92c1e8e696
ErwinA <acuario.erwin00@gmail.com>
2020-09-22 16:28:52 -0700

    Set up library and created main class

---
+++ src/DatabaseTester.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class DatabaseTester
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            //1. Get a connection to database
+
+            //2. Create a statement
+
+            //3. Execute a SQL query
+
+            //4. Process the results set
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
