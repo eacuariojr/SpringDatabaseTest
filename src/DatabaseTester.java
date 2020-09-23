fe6fcf4968aefe5c68d3dcf7cf2d16e9575d2824
ErwinA <acuario.erwin00@gmail.com>
2020-09-22 17:06:55 -0700

    Created basic read query from java to mySQL database
    It worked the first time!

--- src/DatabaseTester.java
+++ src/DatabaseTester.java
@@ -7,12 +7,16 @@ public class DatabaseTester
         try
         {
             //1. Get a connection to database
-
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "password1");
             //2. Create a statement
-
+            Statement myStatement =  myConn.createStatement();
             //3. Execute a SQL query
-
+            ResultSet myRs = myStatement.executeQuery("SELECT * FROM department");
             //4. Process the results set
+            while (myRs.next())
+            {
+                System.out.println(myRs.getString("name") + " " + myRs.getString("position"));
+            }
         }
         catch (Exception e)
         {
